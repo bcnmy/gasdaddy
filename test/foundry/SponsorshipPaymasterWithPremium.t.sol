55d967ddd387b50a2121537f85d3451ecb6aceef
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2024-06-26 13:13:35 +0400

    Setup nexus submodule and fix remappings for foundry test

---
+++ test/foundry/SponsorshipPaymasterWithPremium.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.24;
+import { BiconomySponsorshipPaymaster } from "../../contracts/sponsorship/SponsorshipPaymasterWithPremium.sol";
+import { Test } from "forge-std/src/Test.sol";
+import { StdCheats } from "forge-std/src/StdCheats.sol";
+
+contract SponsorshipPaymasterWithPremiumTest is Test {
+    
+}
