967379cdcfe25fcd89124939d014620704f2c5e8
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2024-06-27 15:04:29 +0400

    Deployment test for BiconomySponsorshipPaymaster

--- test/foundry/SponsorshipPaymasterWithPremium.t.sol
+++ test/foundry/SponsorshipPaymasterWithPremium.t.sol
@@ -1,16 +1,23 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.26;
 
-import { Test } from "forge-std/src/Test.sol";
-import { Vm } from "forge-std/src/Vm.sol";
+import { console2 } from "forge-std/src/Console2.sol";
 
-import { BiconomySponsorshipPaymaster } from "../../contracts/sponsorship/SponsorshipPaymasterWithPremium.sol";
 import { NexusTestBase } from "./base/NexusTestBase.sol";
 
-contract SponsorshipPaymasterWithPremiumTest is NexusTestBase {
-    function setUp() public virtual override {
-        super.setUp();
+import { BiconomySponsorshipPaymaster } from "../../contracts/sponsorship/SponsorshipPaymasterWithPremium.sol";
 
-        
+
+contract SponsorshipPaymasterWithPremiumTest is NexusTestBase {
+    function setUp() public {
+        setupTestEnvironment();
+    }
+
+    function testDeploy() external {
+        BiconomySponsorshipPaymaster testArtifact = new BiconomySponsorshipPaymaster(BOB_ADDRESS, ENTRYPOINT, ALICE_ADDRESS, CHARLIE_ADDRESS);
+        assertEq(address(testArtifact.owner()), BOB_ADDRESS);
+        assertEq(address(testArtifact.entryPoint()), ENTRYPOINT_ADDRESS);
+        assertEq(address(testArtifact.verifyingSigner()), ALICE_ADDRESS);
+        assertEq(address(testArtifact.feeCollector()), CHARLIE_ADDRESS);
     }
 }
