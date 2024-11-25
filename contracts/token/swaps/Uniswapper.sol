1db07d19d7349fb09c4e4f21e6427c122cc24676
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2024-11-25 15:53:16 +0400

    remap clean

--- contracts/token/swaps/Uniswapper.sol
+++ contracts/token/swaps/Uniswapper.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-//import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/IPeripheryPayments.sol";
 
 interface ISwapRouter {
